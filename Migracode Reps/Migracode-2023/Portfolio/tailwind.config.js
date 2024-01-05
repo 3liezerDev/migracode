38e770e365f5d6a40249b0647833722ff9bfdc43
3liezerDev <3dgar3liezer@gmail.com>
2024-01-05 21:54:49 +0100

    portfolio finalizado

--- Migracode Reps/Migracode-2023/Portfolio/tailwind.config.js
+++ Migracode Reps/Migracode-2023/Portfolio/tailwind.config.js
@@ -6,8 +6,13 @@ module.exports = {
       fontFamily: {
         'syne': ['Sansita Swashed', 'Syne'],
         'grotesk': ['Space Grotesk','Dancing Script']
-      }
+      },
+      backgroundImage: theme => ({
+        'gradient-radial': 'radial-gradient(100% 50% at 50% 00%, rgba(0, 163, 255, 0.13) 0, rgba(0, 163, 255, 0) 50%, rgba(0, 163, 255, 0) 100%)',
+      })
     }
   },
-  plugins: []
+  plugins: [
+    require('tailwindcss-animated')
+  ]
 };
